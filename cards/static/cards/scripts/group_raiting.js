3db3ff922226e302b62180df304b92a5bcb11f4d
вадим кожевников <kozevnikovvadim51@gmail.com>
2026-01-09 21:18:02 +0700

    структура скрипта

---
+++ cards/static/cards/scripts/group_raiting.js
@@ -0,0 +1,28 @@
+
+rating_api_url = 'api/raiting_group/'
+selector_id = "raiting__input"
+
+function set_mark(group_id) {
+    let a = document.getElementById(selector_id);
+    let v = Number.parseInt(a.value);
+
+    if (isNaN(v) || v > 5 || v < 1) {
+        console.error('invalid raiting', a.value)
+        return;
+    }
+    try {
+        fetch(rating_api_url, { method: "POST", headers: { "group-id": group_id, "mark": v } })
+            .then(function (v) {
+                if (!v.ok) {
+                    console.error('response error ' + v.status);
+                    return;
+                }
+            })
+            .catch(function (e) {
+                console.error(e);
+            });
+    } catch (e) {
+        console.error(e);
+    }
+
+}
