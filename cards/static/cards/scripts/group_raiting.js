7fb0d847b168deb26f44a1fc1553bc078dbf52b7
вадим кожевников <kozevnikovvadim51@gmail.com>
2026-01-11 15:42:17 +0700

    вынесена функция send_data

--- cards/static/cards/scripts/group_raiting.js
+++ cards/static/cards/scripts/group_raiting.js
@@ -2,16 +2,9 @@
 rating_api_url = 'api/raiting_group/'
 selector_id = "raiting__input"
 
-function set_mark(group_id) {
-    let a = document.getElementById(selector_id);
-    let v = Number.parseInt(a.value);
-
-    if (isNaN(v) || v > 5 || v < 1) {
-        console.error('invalid raiting', a.value)
-        return;
-    }
+function send_data(dat) {
     try {
-        fetch(rating_api_url, { method: "POST", headers: { "group-id": group_id, "mark": v } })
+        fetch(rating_api_url, { method: "POST", headers: dat })
             .then(function (v) {
                 if (!v.ok) {
                     console.error('response error ' + v.status);
@@ -24,6 +17,19 @@ function set_mark(group_id) {
     } catch (e) {
         console.error(e);
     }
+}
+
+function set_mark(group_id) {
+    let a = document.getElementById(selector_id);
+    let v = Number.parseInt(a.value);
+
+    if (isNaN(v) || v > 5 || v < 1) {
+        console.error('invalid raiting', a.value)
+        return;
+    }
+
+    send_data({ "group-id": group_id, "mark": v });
 
 
 }
+
