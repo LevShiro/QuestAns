21b1304cff4bc4c68e6173e0d5855dda6e83cba1
вадим кожевников <kozevnikovvadim51@gmail.com>
2026-01-11 20:55:07 +0700

    примерная структура

--- cards/static/cards/scripts/group_raiting.js
+++ cards/static/cards/scripts/group_raiting.js
@@ -1,6 +1,45 @@
 
 rating_api_url = 'api/raiting_group/'
 selector_id = "raiting__input"
+update_element_id = ''
+
+
+function update_element() {
+    try {
+        resp = await fetch(document.URL);
+    } catch (e) {
+        console.error(e);
+        return;
+    }
+    
+    if (!resp.ok) {
+        console.error('get status code ' + resp.status)
+        return;
+    }
+    try {
+        resptext = await resp.text(); // need for check
+        df = new DocumentFragment();
+        df.appendChild(document.createElement('html')).insertAdjacentHTML('afterbegin', resptext)
+        el = df.getElementById(update_element_id);
+        if (el == null) {
+            console.error('element on getted not exist');
+            return;
+        }
+        tel = document.getElementById(update_element_id)
+        if (tel == null) {
+            console.error('element on page not exist');
+            return;
+        }
+        tel.replaceWith(el);
+
+    } catch (e) {
+
+    }
+
+
+
+}
+
 
 function send_data(dat) {
     try {
