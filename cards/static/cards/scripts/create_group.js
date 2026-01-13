7b11b59500fa291cdf22a9821cbfa959443fc0c0
вадим кожевников <kozevnikovvadim51@gmail.com>
2026-01-13 21:41:00 +0700

    добавлена проверка на атрибут nom который указывает на номер карточки

--- cards/static/cards/scripts/create_group.js
+++ cards/static/cards/scripts/create_group.js
@@ -1,17 +1,31 @@
-const root_elem_id = '';
+const root_elem_id = 'root';
 
 
 
 function add_card() {
 
     let root = document.getElementById(root_elem_id);
-    el = root.lastElementChild;
+    let el = root.lastElementChild;
     root.appendChild(el.cloneNode(true));
     el = root.lastChild;
     let elems_to_reset = el.getElementsByClassName('field')
     for (i of elems_to_reset) {
-        i.value = '';
+        if (i.getAttribute('nom') != null) {
+            n = i.getAttribute('nom');
+            n = Number.parseInt(n);
+            if (isNaN(n)) {
+                n = String(root.childElementCount);
+            } else {
+                n = String(n + 1);
+            }
+            i.setAttribute('nom', n);
+            i.value = n;
+        } else {
+            i.value = '';
+        }
     }
+    
+
 }
 
 
