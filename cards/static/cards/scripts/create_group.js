a241d6e19b9c9790cda3d7ba4076a23ba2af182b
вадим кожевников <kozevnikovvadim51@gmail.com>
2026-01-14 00:14:23 +0700

    подправлено оформление

--- cards/static/cards/scripts/create_group.js
+++ cards/static/cards/scripts/create_group.js
@@ -1,7 +1,7 @@
 const root_elem_id = 'root';
-const api_to_send_card = ''
-const class_name_for_field = 'field'
-const nomer_attribute_name = 'nom'
+const api_to_send_card = '';
+const class_name_for_field = 'field';
+const nomer_attribute_name = 'nom';
 
 
 function add_card() {
@@ -10,7 +10,7 @@ function add_card() {
     let el = root.lastElementChild;
     root.appendChild(el.cloneNode(true));
     el = root.lastChild;
-    let elems_to_reset = el.getElementsByClassName(class_name_for_field)
+    let elems_to_reset = el.getElementsByClassName(class_name_for_field);
     for (i of elems_to_reset) {
         if (i.getAttribute(nomer_attribute_name) != null) {
             n = i.getAttribute(nomer_attribute_name);
@@ -33,26 +33,26 @@ function add_card() {
 
 function send_cards() {
     let root = document.getElementById(root_elem_id);
-    result_data = []
+    result_data = [];
     for (card of root.children) {
         data = new FormData();
-        elems = card.getElementsByClassName(class_name_for_field)
+        elems = card.getElementsByClassName(class_name_for_field);
         for (i of elems) {
             if (i.hasAttribute(nomer_attribute_name)) continue;
             
             if (i.getAttribute('type') != null && i.getAttribute('type') == 'file') {
                 for (f of i.files) {
-                    data.append(i.getAttribute('name'), f)
+                    data.append(i.getAttribute('name'), f);
                 }
                 continue;
             }
             
-            data.append(i.getAttribute('name'), i.getAttribute('value'))
+            data.append(i.getAttribute('name'), i.getAttribute('value'));
         }
         result_data.push(data)
     }
-    fetch(api_to_send_card, { method: "POST", headers: {'X-CSRFToken': Cookies.get('csrftoken') }, body: JSON.stringify(result_data)})
-    .catch(console.error)
+    fetch(api_to_send_card, { method: "POST", headers: { 'X-CSRFToken': Cookies.get('csrftoken') }, body: JSON.stringify(result_data) })
+        .catch(console.error);
 }
 
 
