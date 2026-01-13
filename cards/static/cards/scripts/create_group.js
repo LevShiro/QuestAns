825d115341573d7b792e29e5a354e9d5df4b72c4
вадим кожевников <kozevnikovvadim51@gmail.com>
2026-01-14 04:10:41 +0700

    добавлена функция удаления карточки

--- cards/static/cards/scripts/create_group.js
+++ cards/static/cards/scripts/create_group.js
@@ -3,9 +3,19 @@ const api_to_send_card = '';
 const class_name_for_field = 'field';
 const nomer_attribute_name = 'nom';
 const name_input_id = 'create_group__input-title';
+const delete_button_class = 'create_cart__delete_button';
+
+function add_del_event(e) {
+    b.addEventListener('click', function () {
+        on_delete_button_pressed(e);
+    });
+
+}
+
+
 
 function add_card() {
-    
+
     let root = document.getElementById(root_elem_id);
     let el = root.lastElementChild;
     root.appendChild(el.cloneNode(true));
@@ -26,7 +36,11 @@ function add_card() {
             i.value = '';
         }
     }
-    
+    let b = el.getElementsByClassName(delete_button_class);
+    if (b[0] != null) {
+        add_del_event(b);
+    }
+
 }
 
 function send_cards() {
@@ -43,14 +57,14 @@ function send_cards() {
         elems = card.getElementsByClassName(class_name_for_field);
         for (i of elems) {
             if (i.hasAttribute(nomer_attribute_name)) continue;
-            
+
             if (i.getAttribute('type') != null && i.getAttribute('type') == 'file') {
                 for (f of i.files) {
                     data.append(i.getAttribute('name'), f);
                 }
                 continue;
             }
-            
+
             data.append(i.getAttribute('name'), i.getAttribute('value'));
         }
         result_data.push(data)
@@ -62,4 +76,39 @@ function send_cards() {
         .catch(console.alert);
 }
 
+function on_delete_button_pressed(e) {
+    let elem = e.nextElementSibling;
+    while (elem != null) {
+        for (i of elem.getElementsByClassName(class_name_for_field)) {
+            if (i.hasAttribute(nomer_attribute_name)) {
+                let n = i.getAttribute(nomer_attribute_name);
+                n = Number.parseInt(n);
+                if (isNaN(n)) {
+                    break;
+                } else {
+                    n = String(n - 1);
+                }
+                i.setAttribute(nomer_attribute_name, n);
+                i.innerText = n;
+                break;
+            }
 
+        }
+
+        elem = elem.nextElementSibling;
+    }
+
+}
+
+function init() {
+    let root = document.getElementById(root_elem_id);
+    let el = root.lastElementChild;
+    let b = el.getElementsByClassName(delete_button_class);
+    if (b[0] != null) {
+        add_del_event(b);
+    }
+
+}
+
+
+init();
