65c33141099765f46db51111c4ead10ff954f9f1
вадим кожевников <kozevnikovvadim51@gmail.com>
2026-01-22 19:47:40 +0700

    добавлена функция next

--- cards/static/cards/scripts/reshalka.js
+++ cards/static/cards/scripts/reshalka.js
@@ -24,14 +24,14 @@ function get_root() {
 
 function update() {
     if (cur_card != fetch_card && !fetching) {
-        send_ansver();
+        save_ansver();
         reset_element();
         make_request();
     }
-
+    canncel_send_data();
 }
 
-function send_ansver(grp_id) {
+function save_ansver(grp_id) {
     if (grp_id != null) group_id = grp_id;
     el = document.getElementById(ansver_id_name);
     if (el == null) {
@@ -40,7 +40,6 @@ function send_ansver(grp_id) {
     }
     ansver = el.value;
     ansvers[fetch_card] = ansver;
-
 }
 
 function make_request() {
@@ -50,6 +49,12 @@ function make_request() {
         fetch(api_to_get_group, { headers: { 'card-id': fetch_card, 'group-id': group_id} })
             .then(function (v) {
                 if (!v.ok) {
+                    if (v.status == 416) {
+                        cur_card = '1';
+                        fetching = false;
+                        update();
+                        throw "card id out of range";
+                    }
                     //reaction to incorrect status code
                     alert('response error ' + v.status);
                     return '';
@@ -100,7 +105,7 @@ function on_chose_another_card(nom, grp_id) {
 
 function submit_send_answers(grp_id) {
     if (grp_id != null) group_id = grp_id;
-    send_ansver();
+    save_ansver();
     fetch(api_to_send_ansver, { method: "POST", headers: { 'X-CSRFToken': Cookies.get('csrftoken'), 'group-id': group_id }, body: JSON.stringify(ansvers) })
         .then(function (resp) {
             if (!resp.ok) {alert('server error code ' + resp.status); return; }
@@ -132,4 +137,12 @@ function send_answers(grp_id) {
     if (grp_id != null) group_id = grp_id;
     for (i of document.getElementsByClassName(submit_class_name)) { i.removeAttribute("hidden"); }
 
+}
+
+function next(grp_id) {
+    if (grp_id != null) group_id = grp_id;
+    let a = Number(cur_card);
+    if (isNaN(a)) a = 0;
+    cur_card = String(a + 1)
+    update();
 }
